57baa816e7d7c5501cad464299d8941c708e2e3e
Roseeeeeeeeeeeeeeeeee <165487175+Roseeeeeeeeeeeeeeeeee@users.noreply.github.com>
2025-05-15 20:43:44 +0800

    登录页样式完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,52 +1,52 @@
 <template>
   <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
+    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on"
+      label-position="left">
 
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title">个人空间后台系统</h3>
       </div>
-
-      <el-form-item prop="username">
+      <!-- 管理员账号 -->
+      <el-form-item prop="loginId">
         <span class="svg-container">
           <svg-icon icon-class="user" />
         </span>
-        <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="Username"
-          name="username"
-          type="text"
-          tabindex="1"
-          auto-complete="on"
-        />
+        <el-input ref="loginId" v-model="loginForm.loginId" placeholder="请输入管理员账号" name="loginId" type="text"
+          tabindex="1" auto-complete="on" />
       </el-form-item>
-
-      <el-form-item prop="password">
+      <!-- 管理员密码 -->
+      <el-form-item prop="loginPwd">
         <span class="svg-container">
           <svg-icon icon-class="password" />
         </span>
-        <el-input
-          :key="passwordType"
-          ref="password"
-          v-model="loginForm.password"
-          :type="passwordType"
-          placeholder="Password"
-          name="password"
-          tabindex="2"
-          auto-complete="on"
-          @keyup.enter.native="handleLogin"
-        />
+        <el-input :key="passwordType" ref="loginPwd" v-model="loginForm.loginPwd" :type="passwordType"
+          placeholder="请输入管理员密码" name="password" tabindex="2" auto-complete="on" @keyup.enter.native="handleLogin" />
         <span class="show-pwd" @click="showPwd">
           <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
         </span>
       </el-form-item>
+      <!-- 验证码表项 -->
+      <div class="captcha">
+        <el-form-item prop="captcha" class="input">
+          <span class="svg-container">
+            <svg-icon icon-class="nested" />
+          </span>
+          <el-input ref="captcha" v-model="loginForm.captcha" placeholder="请输入验证码" name="captcha" type="text"
+            tabindex="3" auto-complete="on" />
+        </el-form-item>
+        <div class="captcha-img" v-html="captchaSVG" @click="getCaptchaFunc"></div>
+      </div>
+      <!--   7天内免登录 -->
+      <el-checkbox v-model="loginForm.checked" class="checkbox">7天内免登录</el-checkbox>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
+      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;"
+        @click.native.prevent="handleLogin">登陆</el-button>
 
-      <div class="tips">
+      <!-- <div class="tips">
         <span style="margin-right:20px;">username: admin</span>
         <span> password: any</span>
-      </div>
+      </div> -->
+      
 
     </el-form>
   </div>
@@ -54,11 +54,12 @@
 
 <script>
 import { validUsername } from '@/utils/validate'
-
+import { getCaptcha } from '@/api/login'
 export default {
   name: 'Login',
   data() {
     const validateUsername = (rule, value, callback) => {
+
       if (!validUsername(value)) {
         callback(new Error('Please enter the correct user name'))
       } else {
@@ -74,27 +75,54 @@ export default {
     }
     return {
       loginForm: {
-        username: 'admin',
-        password: '111111'
+        loginId: '',
+        loginPwd: '',
+        captcha: '',
+        checked:true
       },
       loginRules: {
-        username: [{ required: true, trigger: 'blur', validator: validateUsername }],
-        password: [{ required: true, trigger: 'blur', validator: validatePassword }]
+        loginId: [
+          {
+            required: true,
+            trigger: 'blur',
+            message: '账号不得为空'
+          }
+        ],
+        loginPwd: [{
+          required: true,
+          trigger: 'blur',
+          message: '密码不得为空'
+        }],
+        captcha: [
+          {
+            required: true,
+            trigger: 'blur',
+            message: '验证码不得为空'
+          }
+        ]
       },
       loading: false,
       passwordType: 'password',
-      redirect: undefined
+      redirect: undefined,
+      captchaSVG: '',
     }
   },
   watch: {
     $route: {
-      handler: function(route) {
+      handler: function (route) {
         this.redirect = route.query && route.query.redirect
       },
       immediate: true
     }
   },
   methods: {
+    async getCaptchaFunc() {
+      const res = await getCaptcha();
+
+
+      this.captchaSVG = res
+
+    },
     showPwd() {
       if (this.passwordType === 'password') {
         this.passwordType = ''
@@ -102,7 +130,7 @@ export default {
         this.passwordType = 'password'
       }
       this.$nextTick(() => {
-        this.$refs.password.focus()
+        this.$refs.loginPwd.focus()
       })
     },
     handleLogin() {
@@ -121,16 +149,20 @@ export default {
         }
       })
     }
+  },
+  created() {
+    this.getCaptchaFunc()
   }
 }
+
 </script>
 
 <style lang="scss">
 /* 修复input 背景不协调 和光标变色 */
 /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
 
-$bg:#283443;
-$light_gray:#fff;
+$bg: #283443;
+$light_gray: #fff;
 $cursor: #fff;
 
 @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
@@ -173,9 +205,9 @@ $cursor: #fff;
 </style>
 
 <style lang="scss" scoped>
-$bg:#2d3a4b;
-$dark_gray:#889aa4;
-$light_gray:#eee;
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
 
 .login-container {
   min-height: 100%;
@@ -234,5 +266,23 @@ $light_gray:#eee;
     user-select: none;
   }
 }
-</style>
 
+.captcha {
+  display: flex;
+
+  .input {
+    width: 70%;
+  }
+
+  .captcha-img {
+    width: 150px;
+
+    height: 50px;
+    margin-left: 5px;
+  }
+ 
+}
+.checkbox{
+    margin-bottom: 20px ;
+  }
+</style>
