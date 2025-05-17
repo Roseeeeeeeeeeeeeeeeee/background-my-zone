dd0986516423be99e070513690ee092437507be1
Roseeeeeeeeeeeeeeeeee <165487175+Roseeeeeeeeeeeeeeeeee@users.noreply.github.com>
2025-05-17 11:42:31 +0800

    更正token存储条件

--- src/utils/request.js
+++ src/utils/request.js
@@ -43,7 +43,10 @@ service.interceptors.response.use(
   response => {
     const res = response.data
     const headers = response.headers
-    localStorage.adminToken = headers.authentication
+    if(headers.authentication){
+      localStorage.adminToken = headers.authentication
+    }
+    
     return res
     // // if the custom code is not 20000, it is judged as an error.
     // if (res.code !== 20000) {
